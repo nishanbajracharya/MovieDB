2117dfaef8560f5e58833ecb2a89a874463a4bc0
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2017-04-24 19:39:12 +0545

    Add list view dummy items

--- app/src/main/java/com/nishan/moviedb/MainActivity.java
+++ app/src/main/java/com/nishan/moviedb/MainActivity.java
@@ -3,8 +3,15 @@ package com.nishan.moviedb;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final Movie[] moviesSkeleton = {
+            new Movie("", "Logan", "Action, Superhero", "9.3/10", "Long Movie Plot Here", "", "", "", "", "", "", "", ""),
+            new Movie("", "", "", "", "", "", "", "", "", "", "", "", ""),
+            new Movie("", "", "", "", "", "", "", "", "", "", "", "", "")
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +19,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+
+        ListView movieList = (ListView) findViewById(R.id.movieListItem);
+
+        movieList.setAdapter(new MovieListAdapter(this, moviesSkeleton));
     }
 }
