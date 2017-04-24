659aaf31d3525d3a5d7d3532cea82bb607d98199
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2017-04-24 19:44:52 +0545

    Add download image utility

---
+++ app/src/main/java/com/nishan/moviedb/DownloadImage.java
@@ -0,0 +1,34 @@
+package com.nishan.moviedb;
+
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+
+public class DownloadImage extengds AsyncTask<String, Void, Bitmap> {
+    ImageView imageView;
+
+    public DownloadImage(ImageView imageView) {
+        this.imageView = imageView;
+    }
+
+    protected Bitmap doInBackground(String... urls) {
+        String imageURL = urls[0];
+        Bitmap bimage = null;
+        try {
+            InputStream in = new java.net.URL(imageURL).openStream();
+            bimage = BitmapFactory.decodeStream(in);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bimage;
+    }
+
+    protected void onPostExecute(Bitmap result) {
+        imageView.setImageBitmap(result);
+    }
+}
